3d6475d6a5692d2f1d1c3ef34bc3bb4c8fd50fc5
Chris <aqpfarmer@gmail.com>
2026-06-03 16:29:33 +0000

    Added ICO for the EXE taskbar and title bar

--- RestaurantPicker/MainWindow.xaml.cs
+++ RestaurantPicker/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
+using System.IO;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using WinRT.Interop;
 
 namespace RestaurantPicker;
 
@@ -7,5 +11,20 @@ public sealed partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        SetWindowIcon();
+    }
+
+    private void SetWindowIcon()
+    {
+        var iconPath = Path.Combine(AppContext.BaseDirectory, "Assets", "AppIcon.ico");
+        if (!File.Exists(iconPath))
+        {
+            return;
+        }
+
+        var hwnd = WindowNative.GetWindowHandle(this);
+        var windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
+        var appWindow = AppWindow.GetFromWindowId(windowId);
+        appWindow.SetIcon(iconPath);
     }
 }
